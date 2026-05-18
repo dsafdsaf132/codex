1b45d642b036a225784ce664766c8befb2a3ed51
dsafdsaf132 <dsafdsaf132@gmail.com>
2026-05-18 17:09:48 +0900

    ci: disable remote downloader when BuildBuddy key is missing

--- .github/scripts/run-bazel-query-ci.sh
+++ .github/scripts/run-bazel-query-ci.sh
@@ -65,6 +65,13 @@ if [[ -n "${BUILDBUDDY_API_KEY:-}" ]]; then
     "--config=${ci_config}"
     "--remote_header=x-buildbuddy-api-key=${BUILDBUDDY_API_KEY}"
   )
+else
+  bazel_query_args+=(
+    --remote_cache=
+    --remote_executor=
+    --bes_backend=
+    --experimental_remote_downloader=
+  )
 fi
 
 if [[ -n "${BAZEL_REPO_CONTENTS_CACHE:-}" ]]; then
